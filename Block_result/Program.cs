334c4cc4748971c28dadf8f0d1d21d8fc9288161
Sarmaon <patratiyaa@gmail.com>
2023-02-19 18:21:01 +0800

    result example

--- Block_result/Program.cs
+++ Block_result/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача: Написать программу, которая из меющего  массива строк формируем массив из строк, длина которых меньше либо равна 3 симвоволам. Первоначальный массив можно ввесли с клавиатуры,
+// либо задать на старте выполнения алгоритма.
+
+string[] mainString = MainStringArray();
+PrintArray(mainString);
+Console.Write("->");
+int count = CountNewLenght(mainString);
+string[] newString = NewStringArray(mainString, count);
+PrintArray(newString);
+
+string[] MainStringArray()
+{
+    string[] arrayString = new string [] {"RHBZ", "SU35", "RPG", "+", "T90", "RVSN", "Army"};
+    return arrayString;
+}
+
+int CountNewLenght(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3) count += 1;
+    }
+    return count;
+}
+
+string[] NewStringArray(string[]array, int lenght)
+{
+    string[] newStringArray = new string[lenght];
+    int j =0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            newStringArray[j] = array[i];
+            j++;
+        }
+    }
+    return newStringArray;
+}
+
+void PrintArray(string[] arr)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write(arr[i] + ", ");
+        else Console.Write(arr[i]);
+    }
+    Console.Write(" ]");
+}
